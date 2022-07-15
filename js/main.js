03050ac85371adb8a0c3293d6ef595a2a99e5cde
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-15 18:19:28 +0200

    style: ultime modifche + rimossi debug in js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ let usedNumbers = [];
 let numbers = 0;
 let div;
 let score = 0;
-userSelectedNumbers = [];
 
 for (let i = 0; i < 5; i++) {
     numbers = uniqueRandomNumber (usedNumbers, 1, 100);
@@ -12,40 +11,33 @@ for (let i = 0; i < 5; i++) {
     div.append(numbers);
     wrapperDom.append(div);
 }
-console.log(usedNumbers); //!debug
 
 setTimeout(function () {
     hide(wrapperDom);
-}, 3000); //! cambiare il tempo di debug
+}, 30000);
 
 setTimeout(function(){
     let userPrompt = parseInt(prompt('inserisci numeri'));
         check(usedNumbers, userPrompt);
-    console.log(userPrompt); //!debug
 
     let userPrompt2 = parseInt(prompt('inserisci numeri'));
         check(usedNumbers, userPrompt2);
-    console.log(userPrompt2); //!debug
 
     let userPrompt3 = parseInt(prompt('inserisci numeri'));
         check(usedNumbers, userPrompt3);
-    console.log(userPrompt3); //!debug
 
     let userPrompt4 = parseInt(prompt('inserisci numeri'));
         check(usedNumbers, userPrompt4);
-    console.log(userPrompt4); //!debug
 
     let userPrompt5 = parseInt(prompt('inserisci numeri'));
         check(usedNumbers, userPrompt5);
-    console.log(userPrompt5); //!debug
 
-    console.log(userSelectedNumbers);
     if (score <= 3) {
         document.getElementById('title').innerHTML = `Hai indovinato ${score} numero/i su 5`; 
     } else if (score > 3) {
         document.getElementById('title').innerHTML = `Hai indovinato ${score} numero/i su 5, ottima memoria!`;
     }
-}, 3001);//! cambiare il tempo di debug
+}, 30001);
 
 
 
