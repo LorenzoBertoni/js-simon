67d533586a834d1c8192c0812b0696b4e777b4f5
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-07-15 17:02:38 +0200

    add : funzione che chiede 5 valori numerici interi + funzione che controlla se il valore inserito corrisponde ad uno dei numeri generati e aumenta lo score di 1.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
     Genero 5 numeri casuali non ripetuti e li stampo nell'DOM;
     Faccio partire un timer di 30 secondi;
     Richiedo all'utente di inserire i numeri visualizzati precedentemente tramite prompt;
-    Imposto un contatore che aumenta ogni volta che l'utente inserisce un numero corretto;
+    Imposto un contatore-->(usedNumbers, score)
+        ?SE il numero inserito dall'utente corrisponde ad uno generato casualmente
+            il contatore aumenta d 1;
     Stampo il risultato del contatore nel DOM.
--- js/main.js
+++ js/main.js
@@ -2,19 +2,55 @@ let wrapperDom = document.getElementById('wrapper');
 let usedNumbers = [];
 let numbers = 0;
 let div;
+let score = 0;
+userSelectedNumbers = [];
 
 for (let i = 0; i < 5; i++) {
     numbers = uniqueRandomNumber (usedNumbers, 1, 100);
     usedNumbers.push(numbers);
-    console.log(numbers);
     div = document.createElement('div');
     div.append(numbers);
     wrapperDom.append(div);
 }
+console.log(usedNumbers); //!debug
 
 setTimeout(function () {
     hide(wrapperDom);
 }, 3000); //! cambiare il tempo di debug
+
+setTimeout(function(){
+    let userPrompt = parseInt(prompt('inserisci numeri'));
+        check(usedNumbers, userPrompt);
+    console.log(userPrompt); //!debug
+
+    let userPrompt2 = parseInt(prompt('inserisci numeri'));
+        check(usedNumbers, userPrompt2);
+    console.log(userPrompt2); //!debug
+
+    let userPrompt3 = parseInt(prompt('inserisci numeri'));
+        check(usedNumbers, userPrompt3);
+    console.log(userPrompt3); //!debug
+
+    let userPrompt4 = parseInt(prompt('inserisci numeri'));
+        check(usedNumbers, userPrompt4);
+    console.log(userPrompt4); //!debug
+
+    let userPrompt5 = parseInt(prompt('inserisci numeri'));
+        check(usedNumbers, userPrompt5);
+    console.log(userPrompt5); //!debug
+
+    console.log(userSelectedNumbers);
+    if (score <= 3) {
+        document.getElementById('title').innerHTML = `Hai indovinato ${score} numero/i su 5`; 
+    } else if (score > 3) {
+        document.getElementById('title').innerHTML = `Hai indovinato ${score} numero/i su 5, ottima memoria!`;
+    }
+}, 3001);//! cambiare il tempo di debug
+
+
+
+
+
 
 
 
@@ -27,7 +63,7 @@ function uniqueRandomNumber(usedNumberList, min, max) {
     let createdNumber;
 
     while (validNumber == false) {
-        createdNumber = randomNumber (min, max);
+        createdNumber = randomNumber(min, max);
 
         if (usedNumberList.includes(createdNumber) == false) {
             validNumber = true;
@@ -40,4 +76,10 @@ function uniqueRandomNumber(usedNumberList, min, max) {
 function hide (element) {
     element.classList.add('d-none');
     return element;
+}
+
+function check (usedNumbers, userPrompt) {
+    if(usedNumbers.includes(userPrompt)){
+        score++;
+    }
 }
